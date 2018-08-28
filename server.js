9d2b65f6a140f31fc9decf8d02bed49b67de7908
Candice 'canned-ice' Thomas <candicebthomas@gmail.com>
2018-08-28 12:28:45 -0700

    fixed server, now runs!

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const pg = require('pg');
 const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
-const constring = 'postgres://tbfoxjsvccjhpw:acb4781a3a9d288b343884eef5a8b95a79ea38864cd483f6eef9adc93b76049a@ec2-54-235-86-226.compute-1.amazonaws.com:5432/dd0mkqadc3k5r';
+
+// 'constring' first one is for deploying in Heroku, second is for running locally
+// const constring = 'postgres://tbfoxjsvccjhpw:acb4781a3a9d288b343884eef5a8b95a79ea38864cd483f6eef9adc93b76049a@ec2-54-235-86-226.compute-1.amazonaws.com:5432/dd0mkqadc3k5r';
+const constring = 'postgres://localhost:5432/books_app';
+
 const client = new pg.Client(constring);
 
 client.connect();
@@ -14,3 +18,4 @@ client.on('error', err => {
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
+app.listen(PORT, () => console.log(`Server has started on PORT ${PORT}!`));
