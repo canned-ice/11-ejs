6f844fe95c7562ccb1512d902635150d9336e487
Candice 'canned-ice' Thomas <candicebthomas@gmail.com>
2018-08-28 13:11:52 -0700

    resolved constring for heroku

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 app.set('view engine', 'ejs');
 
 // 'constring' first one is for deploying in Heroku, second is for running locally
-// const constring = 'postgres://tbfoxjsvccjhpw:acb4781a3a9d288b343884eef5a8b95a79ea38864cd483f6eef9adc93b76049a@ec2-54-235-86-226.compute-1.amazonaws.com:5432/dd0mkqadc3k5r';
-const constring = 'postgres://localhost:5432/books_app';
+const constring = 'postgres://tbfoxjsvccjhpw:acb4781a3a9d288b343884eef5a8b95a79ea38864cd483f6eef9adc93b76049a@ec2-54-235-86-226.compute-1.amazonaws.com:5432/dd0mkqadc3k5r';
+// const constring = 'postgres://localhost:5432/books_app';
 const client = new pg.Client(constring);
 
 client.connect();
