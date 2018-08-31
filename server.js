a3ceb7aa4d0d42c20676a970818185ed7a90cf4f
Candice 'canned-ice' Thomas <candicebthomas@gmail.com>
2018-08-31 12:58:24 -0700

    added superagent

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 require('dotenv').config();
 const pg = require('pg');
 const express = require('express');
+const superagent = require('superagent');
 const PORT = process.env.PORT || 8080; 
 const app = express();
 const constring = process.env.DATABASE_URL || 'postgres://localhost:5432/books_app';
@@ -18,60 +19,59 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', showHome);
 app.get('/book/:id', showBook);
-app.get('/new', showForm);
+app.get('/form', showForm);
 
-app.post('/new/submit', addBook);
+app.post('/form/submit', addBook);
 
 app.listen(PORT, () => console.log(`Server has started on PORT ${PORT}!`));
 app.use('*', (req, res) => res.render('error') );
 
 
 // Show the Home index page upon visit of the site
-function showHome(request, response) {
+function showHome(req, res) {
   let SQL = 'SELECT title, author, image_url, id FROM books'
   client.query(SQL)
   .then( data => {
     let booklist = data.rows;
-    response.render('index', {items: booklist})
-    console.log('going thru get.then');
+    res.render('index', {items: booklist})
   })
   .catch(err => {
     console.error(err);
-    response.render('error');
+    res.render('error');
   })
 };
 
 // Show a single book's details after clicking link from index
-function showBook( request, response ) {
+function showBook( req, res ) {
   let SQL =`SELECT * FROM books WHERE id=$1`;
-  let id = request.params.id;
+  let id = req.params.id;
   let values = [id];
   client.query(SQL, values)
   .then( data =>{
-    response.render('book', {item:data.rows[0]})
+    res.render('book', {item:data.rows[0]})
   })
 };
 
 // Show the add-book form
-function showForm( request, response ) {
-  response.render('new');
+function showForm( req, res ) {
+  res.render('form');
 };
 
-// Add a book's details to database AFTER clicking submit from /new
-function addBook( request, response ){
+// Add a book's details to database AFTER clicking submit from /form
+function addBook( req, res ){
   let SQL = `INSERT INTO books (title, author, image_url, description, isbn) VALUES ($1, $2, $3, $4, $5)`;
   let values = [
-    request.body.title,
-    request.body.author,
-    request.body.image_url,
-    request.body.description,
-    request.body.isbn
+    req.body.title,
+    req.body.author,
+    req.body.image_url,
+    req.body.description,
+    req.body.isbn
   ];
   client.query(SQL, values)
     .then( () => {
       let newBookData = [];
-      newBookData.push(request.body);
-      response.render('add', {
+      newBookData.push(req.body);
+      res.render('add', {
         item: newBookData});
     });
 }
